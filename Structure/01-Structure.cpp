f272bb8f1b1858ad669854712455f8b25b39ee74
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-07-08 13:54:23 +0530

    Structure Basic concept & Ques

--- Structure/01-Structure.cpp
+++ Structure/01-Structure.cpp
@@ -14,6 +14,13 @@ int main()
 {
     // declaration and initialization
     struct Rectangle r1 = {10, 15};
+
+    // change value method
+
+    r1.length = 15;
+    r1.breadth = 25;
+    cout << r1.length << endl;
+    cout << r1.breadth << endl;
     cout << " Size of member " << sizeof(r1) << endl;
 
     return 0;
