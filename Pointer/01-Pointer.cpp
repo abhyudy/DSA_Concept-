bac2c46afdb3a717d51e5b46731cb1811b94a776
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-07-09 23:10:58 +0530

    pointer basic to advance concepts & Question

---
+++ Pointer/01-Pointer.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int *p; // * is only used for declaration of pointer
+    p = &a; // & is used for address
+
+    cout << a << endl;
+    cout << "using pointer " << *p << endl; // *p means that it go to that addrress and take value
+    return 0;
+}
